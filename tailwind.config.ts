595937be5b54eb143f0efc7cf5ad56108135bec6
TaeHyun <Taehyun>
2025-06-08 10:07:29 -0700

    chore: base style & shadcn init

---
+++ tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: "class",
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        "primary-foreground": "var(--primary-foreground)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config 
